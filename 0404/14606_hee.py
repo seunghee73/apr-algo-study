61fed9aa462dac5e99f2d8a755b2cee109b9e75b
seunghee <cshhhh16@gmail.com>
2022-04-04 19:48:48 +0900

    0404 hee

--- 0404/14606_hee.py
+++ 0404/14606_hee.py
@@ -2,10 +2,6 @@ N = int(input())
 dp = [0] * (N+1)
 
 for i in range(1, N+1):
-    if i == 1:
-        dp[i] = 0
-    else:
-        dp[i] = dp[i-1] + (i-1)
-
+    dp[i] = dp[i-1] + (i-1)
 print(dp[-1])
 
