9d4005bac0f1d12c30510ebb60f970eae6cb08c0
S.Hee <97743926+seunghee73@users.noreply.github.com>
2022-04-08 22:04:16 +0900

    Update 2636_hee.py

--- 0408/2636_hee.py
+++ 0408/2636_hee.py
@@ -10,28 +10,26 @@ for i in range(N):
     MAP.append(l)
 
 D = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-F = [(0, 0), (M-1, 0), (0, N-1), (M-1, N-1)] # 공기와 접족된 칸을 체크 하기 위한 네 꼭짓점
 
 t, temp = 0, cheese
 while True:
     V = [[False] * M for _ in range(N)]
-    for i, j in F:
-        V[j][i] = 0
-        ST = deque([(i, j)])
-        while ST:
-            x, y = ST.popleft()
-            for d in D:
-                nx = x + d[0]
-                ny = y + d[1]
-                if -1 < ny < N and -1 < nx < M and not V[ny][nx]:
-                    if MAP[ny][nx] == 0: # 공기 혹은 공기와 접촉된 칸 ST에 추가
-                        ST.append((nx, ny))
+    V[0][0] = 0
+    ST = deque([(0, 0)])
+    while ST:
+        x, y = ST.popleft()
+        for d in D:
+            nx = x + d[0]
+            ny = y + d[1]
+            if -1 < ny < N and -1 < nx < M and not V[ny][nx]:
+                if MAP[ny][nx] == 0: # 공기 혹은 공기와 접촉된 칸 ST에 추가
+                    ST.append((nx, ny))
 
-                    elif MAP[ny][nx] == 1: # 치즈가 있다면 그 칸의 치즈는 녹는다
-                        MAP[ny][nx] = 0
-                        temp -= 1
+                elif MAP[ny][nx] == 1: # 치즈가 있다면 그 칸의 치즈는 녹는다
+                    MAP[ny][nx] = 0
+                    temp -= 1
 
-                    V[ny][nx] = True
+                V[ny][nx] = True
     t += 1
     if temp == 0: # 치즈가 다 녹았다면 break
         break
