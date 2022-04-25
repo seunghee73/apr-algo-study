0f058c15eacc622df87e29420d8fe00217ccd67f
yuzu <myuju419@naver.com>
2022-04-25 19:52:39 +0900

    0425 yuzu

---
+++ 0425/1446_yuzu.py
@@ -0,0 +1,31 @@
+import heapq
+import collections
+import sys
+input = sys.stdin.readline
+inf = sys.maxsize
+
+n, d = map(int, input().split())
+
+graph = collections.defaultdict(list)
+for _ in range(n):
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w))
+for i in range(d+1):
+    graph[i].append((i+1, 1))
+
+dist = [1e9]*(d+1)
+def dijkstra():
+    q = [(0, 0)]
+    dist[0] = 0
+    while q:
+        t, node = heapq.heappop(q)
+        if dist[node] < t:
+            continue
+        for v, w in graph[node]:
+            if v<=d and  t+w < dist[v]:
+                dist[v] = t+w
+                heapq.heappush(q, (t+w, v))
+    print(dist[-1])
+    return
+
+dijkstra()
